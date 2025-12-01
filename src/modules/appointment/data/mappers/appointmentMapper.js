678f5d3b078a872d11bea9338713b457c938585f
Gerardo Bohorquez <gerardo.sebastian.bl@gmail.com>
2025-12-01 09:19:11 -0500

    Update: mappers appointment

--- src/modules/appointment/data/mappers/appointmentMapper.js
+++ src/modules/appointment/data/mappers/appointmentMapper.js
@@ -18,6 +18,7 @@ export class AppointmentMapper {
              clinicId: dto.clinicId,
              patientFirstName: dto.patientFirstName,
              patientLastName: dto.patientLastName,
+             serviceId: dto.serviceId,
          });
      }
 
@@ -50,6 +51,7 @@ export class AppointmentMapper {
             appointmentDate: data.appointmentDate,
             shiftName: data.shiftName,
             clinicId: data.clinicId,
+            serviceId: data.serviceId,
         };
     }
 
